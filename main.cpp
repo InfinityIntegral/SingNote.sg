dd39d853414a2db1038234a09d119108d84229a4
Infinity Integral <infinityintegral333@gmail.com>
2025-05-19 11:13:06 -0400

    added app icon that is my profile picture

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 #include "sgcentralmanagement.h"
 
 int main(int argc, char** argv){
+    QCoreApplication::setApplicationName("SingNote.sg");
+    QCoreApplication::setApplicationVersion("1 + i");
+    QCoreApplication::setOrganizationName("05524F.sg");
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/assets/05524Flogo.png"));
     SGCentralManagement::initialise();
     return app.exec();
 }
